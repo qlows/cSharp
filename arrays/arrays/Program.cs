6ed29e935f35304e3771be89dbfa79b63589afa6
qlows <79415956+qlows@users.noreply.github.com>
2021-06-05 21:15:27 -0400

    Add files via upload

--- arrays/arrays/Program.cs
+++ arrays/arrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace arrays
 {
@@ -6,7 +7,15 @@ namespace arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 51}; //[] makes it array
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            WriteLine(luckyNumbers[0]);
+
+
+            ReadLine();
         }
     }
 }
