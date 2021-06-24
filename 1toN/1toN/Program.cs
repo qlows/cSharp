a33f004df013f6a6afa3aa2d2a03708644a1305b
qlows <79415956+qlows@users.noreply.github.com>
2021-06-24 14:55:23 -0400

    Add files via upload

---
+++ 1toN/1toN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+namespace _1toN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            Write("Please enter a positive number: ");
+            n = Convert.ToInt32(ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            WriteLine("Sum of numbers from 1 to " + n + " is " + sum);
+            ReadLine();
+        }
+    }
+}
